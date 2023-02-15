98467e88fe603c914883a07a381845310478c962
hyejin30 <tyui3185@gmail.com>
2023-02-15 16:05:11 +0900

    Add : useQuery with variables

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,5 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 function Movie() {
-  return <div>This is Movie Detail</div>;
+  const { id } = useParams();
+
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+
+  if (loading) <h1>Fetching Movie...</h1>;
+  if (error) <h1>Could not fetch :(</h1>;
+  return <div>{data.movie.title}</div>;
 }
 
 export default Movie;
