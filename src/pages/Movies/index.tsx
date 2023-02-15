9874c3e653d430d242a920ef6c514b12a4e9be92
hyejin30 <tyui3185@gmail.com>
2023-02-15 14:50:44 +0900

    Add : graphQL useQuery

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,28 +1,22 @@
-import { useEffect, useState } from "react";
-import { gql, useApolloClient } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+  }
+`;
 
 function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              title
-              id
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <h1>Colud not fetch :(</h1>;
 
   return (
     <div>
-      {movies.map((movie) => (
+      {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
       ))}
     </div>
